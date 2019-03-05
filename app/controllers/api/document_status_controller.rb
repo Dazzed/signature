f42b5580cdb5180b7bf6450b32686766b916e589
Dazzed <sameepsheth@gmail.com>
2019-03-05 14:42:17 +0530

    Bugfixes

--- app/controllers/api/document_status_controller.rb
+++ app/controllers/api/document_status_controller.rb
@@ -7,7 +7,24 @@ class Api::DocumentStatusController < ApplicationController
     if @deal.nil?
       return render json: {"success": false, "message": "Invalid deal id"} 
     end
-    render json: {"success": true, "document_status": @deal.documents}
+
+    deal_documents = []
+    @deal.documents.each do |document|
+      doc = {
+        "_id" => document.id,
+        "client_deal_id" => document.client_deal_id,
+        "parties" => document.parties,
+        "template_id" => document.template_id,
+        "document_title" => document.document_title,
+        "deal_attributes" => document.deal_attributes,
+        "complete" => document.complete,
+        "deal_id" => document.deal_id,
+        "createdAt" => document.createdAt,
+        "apiLink" => document_signature_path(document.parties[0]["signature_request_id"])
+      }
+      deal_documents.push(doc)
+    end
+    render json: {"success": true, "document_status": deal_documents}
   end
   
   private
