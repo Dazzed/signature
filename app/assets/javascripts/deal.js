453b36a5ee2f45e63ad12c702f8f404b4ff6b1e6
circleofawesome <samiul.anwar@gmail.com>
2019-04-02 18:59:56 -0400

    :construction: :scissors: moves js to js file

--- app/assets/javascripts/deal.js
+++ app/assets/javascripts/deal.js
@@ -1,19 +1,51 @@
 function getTemplate(template_id, deal_id) {
   $.ajax({
-    url: '/documents/new?template_id=' + template_id + '&client_deal_id=' + deal_id,
-    success: function (data) {
-      $('#form-container').html(data);
+    url:
+      "/documents/new?template_id=" +
+      template_id +
+      "&client_deal_id=" +
+      deal_id,
+    success: function(data) {
+      $("#form-container").html(data);
     },
-    error: function (err) {
-      console.log('Error in getTemplate');
+    error: function(err) {
+      console.log("Error in getTemplate");
     }
-  })
+  });
   $(".list-group-item").removeClass("list-group-item-active");
-  $(".list-group").find("a[data-template_id='" + template_id + "']").addClass("list-group-item-active"); 
+  $(".list-group")
+    .find("a[data-template_id='" + template_id + "']")
+    .addClass("list-group-item-active");
 }
 
-$(function () {
-  if (location.pathname.indexOf('/deals') !== -1) {
-    $('#deal-details-tab li:nth-child(2) a').tab('show');
+$(function() {
+  if (location.pathname.indexOf("/deals") !== -1) {
+    $("#deal-details-tab li:nth-child(2) a").tab("show");
   }
 });
+
+const nonPreviewTabs = ["#form-tab", "#documents-tab"];
+
+const getPreview = (client, url) => {
+  const $container = $("#document-container");
+  const $previewContainer = $("#previewContainer");
+
+  $("#preview-tab").click(() => {
+    $container.css("display", "none");
+    $previewContainer.css("display", "inherit");
+    HelloSign.init(client);
+    HelloSign.open({
+      url,
+      skipDomainVerification: true,
+      container: document.getElementById("previewContainer"),
+      height: 5500
+    });
+  });
+
+  nonPreviewTabs.forEach(tab => {
+    $(tab).click(e => {
+      $previewContainer.css("display", "none");
+      $container.css("display", "inherit");
+    });
+  });
+};
